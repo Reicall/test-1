7392ad72160b48be9b9152158c5e436a0b5f13b1
Reicall <137397068+Reicall@users.noreply.github.com>
2023-07-06 17:41:29 +0700

    Create randomPassword.py

---
+++ randomPassword.py
@@ -0,0 +1,10 @@
+import random
+
+character = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+pass_length = int(input("Masukkan panjang password"))
+
+password = ""
+for i in range(pass_length):
+    password += random.choice(character)
+
+print(password)
